a8043c83877eb7475401244f48bf4e3bbe69d1dc
Ritwiz <madangopalsingh2803@gmail.com>
2024-10-15 23:47:14 +0530

    brought the custom app bar back

--- lib/screens/alumni_screen.dart
+++ lib/screens/alumni_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_app_bar.dart';
 import 'package:alumni_connect/services/alumni_data.dart';
-import 'package:alumni_connect/widgets/custom_card.dart'; // Import the updated AlumniCard
+import 'package:alumni_connect/widgets/custom_card.dart';
 
 class AlumniScreen extends StatefulWidget {
   @override
@@ -15,11 +15,26 @@ class _AlumniScreenState extends State<AlumniScreen> {
   bool isFetchingMore = false;
   int remaining = 50;
   AlumniData alumniData = AlumniData();
+  ScrollController _scrollController = ScrollController();
 
   @override
   void initState() {
     super.initState();
     fetchAlumniData();
+    _scrollController.addListener(_scrollListener);
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollListener() {
+    if (_scrollController.position.pixels == _scrollController.position.maxScrollExtent) {
+      fetchAlumniData();
+    }
   }
 
   Future<void> fetchAlumniData() async {
@@ -50,40 +65,46 @@ class _AlumniScreenState extends State<AlumniScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: isLoading
-          ? Center(child: CircularProgressIndicator())
-          : NotificationListener<ScrollNotification>(
-              onNotification: (ScrollNotification scrollInfo) {
-                if (!isFetchingMore && scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-                  fetchAlumniData();
-                  return true;
-                }
-                return false;
-              },
-              child: ListView.builder(
-                itemCount: alumniItems.length + (isFetchingMore ? 1 : 0),
-                itemBuilder: (context, index) {
-                  if (index == alumniItems.length) {
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          CustomAppBar(),
+          SliverToBoxAdapter(
+            child: isLoading
+                ? Center(child: CircularProgressIndicator())
+                : Container(), // Empty container if not loading
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                if (index == alumniItems.length) {
+                  if (isFetchingMore) {
                     return Center(child: CircularProgressIndicator());
+                  } else {
+                    return null; // Return null to stop building more items
                   }
-                  var alumni = alumniItems[index];
-                  return AlumniCard(
-                    index: index,
-                    isSelected: selectedCardIndex == index,
-                    name: alumni['NAME'],
-                    company: alumni['COMPANY'],
-                    batch: alumni['BATCH'],
-                    profilePicUrl: alumni['PIC'],
-                    profileLink: alumni['PROFILE'],
-                    onTap: () {
-                      setState(() {
-                        selectedCardIndex = index;
-                      });
-                    },
-                  );
-                },
-              ),
+                }
+                var alumni = alumniItems[index];
+                return AlumniCard(
+                  index: index,
+                  isSelected: selectedCardIndex == index,
+                  name: alumni['NAME'],
+                  company: alumni['COMPANY'],
+                  batch: alumni['BATCH'],
+                  profilePicUrl: alumni['PIC'],
+                  profileLink: alumni['PROFILE'],
+                  onTap: () {
+                    setState(() {
+                      selectedCardIndex = index;
+                    });
+                  },
+                );
+              },
+              childCount: alumniItems.length + (isFetchingMore ? 1 : 0),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
